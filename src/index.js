449d382d4c63c028e41c27b029741f4a6898c508
Vadim <chakapega@gmail.com>
2019-10-07 22:18:45 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+  const arrayForFirst = first.split('').reverse();
+  const arrayForSecond = second.split('').reverse();
+  const stack = [];
+
+  for (let i = 0; i < arrayForFirst.length; i++) {
+    for (let k = 0; k < arrayForSecond.length; k++) {
+      const multiplicationResult = arrayForFirst[i] * arrayForSecond[k];
+
+      stack[i + k] = (stack[i + k]) ? stack[i + k] + multiplicationResult : multiplicationResult;
+    };
+  };
+
+  for (let i = 0; i < stack.length; i++) {
+    const number = stack[i] % 10;
+
+    const transfer = Math.floor(stack[i] / 10);
+
+    stack[i] = number;
+    
+    if (stack[i + 1]) {
+      stack[i + 1] += transfer;
+    } else if (transfer !== 0) {
+      stack[i + 1] = transfer;
+    };
+  };
+  
+  return stack.reverse().join('');
+};
